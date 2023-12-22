ad93a0cdd55ce72b694f7d862787da5f553e9948
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-22 15:03:46 +0545

    feat: build-maintained

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,13 +30,13 @@ const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
-app.use(express.static("build"));
-
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "./build")));
 
 
 app.get("*", (req, res) => {
