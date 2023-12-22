a7ab9b055b6b047c2abd794e772c55421f5de75f
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-22 14:03:15 +0545

    feat: mongoose-updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL), {
+    .connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-    }
+    })
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
+    })
+    .catch((error) => {
+      console.error(`MongoDB connection error: ${error}`);
     });
 };
 
