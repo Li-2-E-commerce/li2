d1dbd583074633203fc0af95e16dd77f93e4f5db
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-22 14:00:35 +0545

    feat: database-changed

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL)
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // useCreateIndex: true,
-
+    .connect(process.env.DB_URL), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
     });
